cca02bec2567c8531df1ae6a94d9f43bcd138f2f
William <williamrldb@gmail.com>
2018-05-18 17:31:52 -0400

    --Diagramando página de busca de restaurantes.

--- rest-web/src/app/restaurants/components/restaurant.component.ts
+++ rest-web/src/app/restaurants/components/restaurant.component.ts
@@ -1,9 +1,17 @@
 import { Component } from "@angular/core";
+import * as jQuery from 'jquery';
 
 @Component({
     selector: 'app-restaurants',
-    templateUrl: './restaurant.component.html'
+    templateUrl: './restaurant.component.html',
+    styleUrls: ['./restaurant.component.css']
 })
 export class RestaurantComponent {
+    ngOnInit(){
+        jQuery('.parallax').parallax();
+    }
 
+    search(e){
+        e.preventDefault();
+    }
 }
