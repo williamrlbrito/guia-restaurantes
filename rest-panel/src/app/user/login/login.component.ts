2ad5e4006754e55b1dd76c052f42d3e5add95400
William <williamrldb@gmail.com>
2018-05-18 10:39:08 -0400

    --Interação da pagina de login.

--- rest-panel/src/app/user/login/login.component.ts
+++ rest-panel/src/app/user/login/login.component.ts
@@ -12,5 +12,39 @@ import { Router } from '@angular/router';
     `]
 })
 export class LoginComponent {
-    
+    user: any = {
+        username: null,
+        password: null
+    };
+
+    constructor(
+        private authService: AuthService,
+        private router: Router
+    ){}
+
+    login(e){
+        e.preventDefault();
+
+        if (!this.user.username || !this.user.password) {
+            window.Materialize.toast('Preencha o email e senha', 3000, 'red');
+            return;
+        }
+
+        let data = {
+            grant_type: 'password',
+            client_id: '',
+            client_secret: '',
+            username: '',
+            password: '',
+            scope: ''
+        }
+
+        this.authService
+            .login(data)
+            .then((res) => {
+                document.cookie = "token=" + res.access_token + "; expires=" + res.expires_in;
+                this.authService.setAccessToken();
+                this.router.navigate(['/']);
+            });
+    }
 }
